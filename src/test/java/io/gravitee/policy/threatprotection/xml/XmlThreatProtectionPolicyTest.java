92f88be09ab8671f892c06c93c580f825ea20e42
Guillaume Cusnieux <guillaume.cusnieux@graviteesource.com>
2022-01-21 15:29:38 +0100

    style: setup & apply prettier

--- src/test/java/io/gravitee/policy/threatprotection/xml/XmlThreatProtectionPolicyTest.java
+++ src/test/java/io/gravitee/policy/threatprotection/xml/XmlThreatProtectionPolicyTest.java
@@ -15,6 +15,10 @@
  */
 package io.gravitee.policy.threatprotection.xml;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.*;
+
 import io.gravitee.common.http.MediaType;
 import io.gravitee.gateway.api.Request;
 import io.gravitee.gateway.api.buffer.Buffer;
@@ -29,10 +33,6 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.mockito.Mockito.*;
 
 /**
  * @author Jeoffrey HAEYAERT (jeoffrey.haeyaert at graviteesource.com)
@@ -53,7 +53,6 @@ public class XmlThreatProtectionPolicyTest {
 
     @Before
     public void before() {
-
         configuration = new XmlThreatProtectionPolicyConfiguration();
         configuration.setMaxChildrenPerElement(100);
         configuration.setMaxDepth(1000);
@@ -65,15 +64,12 @@ public class XmlThreatProtectionPolicyTest {
         configuration.setMaxTextValueLength(100);
         configuration.setMaxLength(1000);
 
-        HttpHeaders httpHeaders = HttpHeaders
-                .create()
-                .add(HttpHeaderNames.CONTENT_TYPE, MediaType.APPLICATION_XML);
+        HttpHeaders httpHeaders = HttpHeaders.create().add(HttpHeaderNames.CONTENT_TYPE, MediaType.APPLICATION_XML);
         when(request.headers()).thenReturn(httpHeaders);
     }
 
     @Test
     public void shouldAcceptAllWhenContentTypeIsNotXml() {
-
         Mockito.reset(request);
         cut = new XmlThreatProtectionPolicy(configuration);
         when(request.headers()).thenReturn(HttpHeaders.create());
@@ -84,7 +80,6 @@ public class XmlThreatProtectionPolicyTest {
 
     @Test
     public void shouldAcceptValidXml() {
-
         cut = new XmlThreatProtectionPolicy(configuration);
         ReadWriteStream<Buffer> readWriteStream = cut.onRequestContent(request, policyChain);
 
@@ -98,7 +93,6 @@ public class XmlThreatProtectionPolicyTest {
 
     @Test
     public void shouldRejectInvalidXml() {
-
         cut = new XmlThreatProtectionPolicy(configuration);
         ReadWriteStream<Buffer> readWriteStream = cut.onRequestContent(request, policyChain);
 
@@ -112,7 +106,6 @@ public class XmlThreatProtectionPolicyTest {
 
     @Test
     public void shouldRejectWhenMaxTextValueLengthExceeded() {
-
         configuration.setMaxTextValueLength(4);
         cut = new XmlThreatProtectionPolicy(configuration);
         ReadWriteStream<Buffer> readWriteStream = cut.onRequestContent(request, policyChain);
@@ -127,7 +120,6 @@ public class XmlThreatProtectionPolicyTest {
 
     @Test
     public void shouldRejectWhenMaxLengthExceeded() {
-
         configuration.setMaxLength(1);
         cut = new XmlThreatProtectionPolicy(configuration);
         ReadWriteStream<Buffer> readWriteStream = cut.onRequestContent(request, policyChain);
@@ -142,14 +134,17 @@ public class XmlThreatProtectionPolicyTest {
 
     @Test
     public void shouldRejectWhenMaxAttributesPerElementExceeded() {
-
         configuration.setMaxAttributesPerElement(1);
         cut = new XmlThreatProtectionPolicy(configuration);
         ReadWriteStream<Buffer> readWriteStream = cut.onRequestContent(request, policyChain);
 
         assertNotNull(readWriteStream);
 
-        readWriteStream.write(Buffer.buffer("<test valid=\"true\" valid2=\"false\" valid3=\"false\" valid4=\"false\" valid5=\"false\" valid6=\"false\" valid7=\"false\" valid8=\"false\" valid9=\"false\">1234</test>"));
+        readWriteStream.write(
+            Buffer.buffer(
+                "<test valid=\"true\" valid2=\"false\" valid3=\"false\" valid4=\"false\" valid5=\"false\" valid6=\"false\" valid7=\"false\" valid8=\"false\" valid9=\"false\">1234</test>"
+            )
+        );
         readWriteStream.end();
 
         verify(policyChain, times(1)).streamFailWith(any(PolicyResult.class));
@@ -157,7 +152,6 @@ public class XmlThreatProtectionPolicyTest {
 
     @Test
     public void shouldRejectWhenMaxChildrenExceeded() {
-
         configuration.setMaxChildrenPerElement(1);
         cut = new XmlThreatProtectionPolicy(configuration);
         ReadWriteStream<Buffer> readWriteStream = cut.onRequestContent(request, policyChain);
@@ -172,7 +166,6 @@ public class XmlThreatProtectionPolicyTest {
 
     @Test
     public void shouldRejectWhenMaxDepthExceeded() {
-
         configuration.setMaxDepth(1);
         cut = new XmlThreatProtectionPolicy(configuration);
         ReadWriteStream<Buffer> readWriteStream = cut.onRequestContent(request, policyChain);
@@ -187,21 +180,24 @@ public class XmlThreatProtectionPolicyTest {
 
     @Test
     public void shouldRejectWhenEntityMaxDepthExceeded() {
-
         configuration.setMaxEntityDepth(4);
         cut = new XmlThreatProtectionPolicy(configuration);
         ReadWriteStream<Buffer> readWriteStream = cut.onRequestContent(request, policyChain);
 
         assertNotNull(readWriteStream);
 
-        readWriteStream.write(Buffer.buffer("<!DOCTYPE test [\n" +
+        readWriteStream.write(
+            Buffer.buffer(
+                "<!DOCTYPE test [\n" +
                 "<!ENTITY lol \"lol\">\n" +
                 "<!ENTITY lol2 \"&lol; &lol;\">\n" +
                 "<!ENTITY lol3 \"&lol2; &lol2;\">\n" +
                 "<!ENTITY lol4 \"&lol3; &lol3;\">\n" +
                 "<!ENTITY lol5 \"&lol4; &lol4;\">\n" +
                 "]>\n" +
-                "<test>&lol5;</test>"));
+                "<test>&lol5;</test>"
+            )
+        );
         readWriteStream.end();
 
         verify(policyChain, times(1)).streamFailWith(any(PolicyResult.class));
@@ -209,18 +205,17 @@ public class XmlThreatProtectionPolicyTest {
 
     @Test
     public void shouldRejectWhenMaxEntitiesExceeded() {
-
         configuration.setMaxEntities(1);
         cut = new XmlThreatProtectionPolicy(configuration);
         ReadWriteStream<Buffer> readWriteStream = cut.onRequestContent(request, policyChain);
 
         assertNotNull(readWriteStream);
 
-        readWriteStream.write(Buffer.buffer("<!DOCTYPE test [\n" +
-                "<!ENTITY lol \"lol\">\n" +
-                "<!ENTITY lol2 \"&lol; &lol;\">\n" +
-                "]>\n" +
-                "<test>&lol2;</test>"));
+        readWriteStream.write(
+            Buffer.buffer(
+                "<!DOCTYPE test [\n" + "<!ENTITY lol \"lol\">\n" + "<!ENTITY lol2 \"&lol; &lol;\">\n" + "]>\n" + "<test>&lol2;</test>"
+            )
+        );
         readWriteStream.end();
 
         verify(policyChain, times(1)).streamFailWith(any(PolicyResult.class));
@@ -228,7 +223,6 @@ public class XmlThreatProtectionPolicyTest {
 
     @Test
     public void shouldRejectWhenMaxElementsExceeded() {
-
         configuration.setMaxElements(2);
         cut = new XmlThreatProtectionPolicy(configuration);
         ReadWriteStream<Buffer> readWriteStream = cut.onRequestContent(request, policyChain);
@@ -243,7 +237,6 @@ public class XmlThreatProtectionPolicyTest {
 
     @Test
     public void shouldRejectExternalEntities() {
-
         configuration.setAllowExternalEntities(false);
         cut = new XmlThreatProtectionPolicy(configuration);
         ReadWriteStream<Buffer> readWriteStream = cut.onRequestContent(request, policyChain);
@@ -253,9 +246,15 @@ public class XmlThreatProtectionPolicyTest {
         // Perform an injection of logback xml file.
         String path = getClass().getResource("/logback-test.xml").getPath();
 
-        readWriteStream.write(Buffer.buffer("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-                "<!DOCTYPE foo [ <!ENTITY xxe SYSTEM \"file://"+path+"\"> ]>\n" +
-                "<stockCheck><productId>&xxe;</productId></stockCheck>"));
+        readWriteStream.write(
+            Buffer.buffer(
+                "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<!DOCTYPE foo [ <!ENTITY xxe SYSTEM \"file://" +
+                path +
+                "\"> ]>\n" +
+                "<stockCheck><productId>&xxe;</productId></stockCheck>"
+            )
+        );
         readWriteStream.end();
 
         verify(policyChain, times(1)).streamFailWith(any(PolicyResult.class));
